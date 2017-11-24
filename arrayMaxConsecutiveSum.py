fa2c82e805149756be5856713c7f5f116f5b2690
JulienRioux <julien.rioux@hotmail.com>
2017-11-24 12:52:11 -0500

    Update arrayMaxConsecutiveSum.py

--- arrayMaxConsecutiveSum.py
+++ arrayMaxConsecutiveSum.py
@@ -1,16 +1,31 @@
+# Given array of integers, find the maximal possible sum of some of its k consecutive elements.
 
+# Example
 
+# For inputArray = [2, 3, 5, 1, 6] and k = 2, the output should be
+# arrayMaxConsecutiveSum(inputArray, k) = 8.
+# All possible sums of 2 consecutive elements are:
 
+# 2 + 3 = 5;
+# 3 + 5 = 8;
+# 5 + 1 = 6;
+# 1 + 6 = 7.
+# Thus, the answer is 8.
 
 def arrayMaxConsecutiveSum(inputArray, k):
-    
+    # keep in memory the maximal Sum
     maxSum = sum(inputArray[0:k])
+    # Copy in testSum the maxSum which is the first tested sum of the input array 
     testSum = maxSum
-    
+    # Iterate over the array (k at a time)
     for i in range(len(inputArray)-k):
-        testSum += inputArray[i+k] 
-        testSum -=  inputArray[i]
-        lastSum = testSum
+        (
+        # To avoid to make the sum of the element in the array at
+        # each iteration; add the next element and substract the 
+        # last element present in the last sum
+        testSum += inputArray[i+k] - inputArray[i]
+        # Check if the testSum is bigger than the maxSum up to date
+        # If bigger, testSum is the new maxSum
         if testSum > maxSum:
                     maxSum = testSum
     
