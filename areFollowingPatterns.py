90371b1286cc9dab260d04c9a91a95464f2f4417
JulienRioux <julien.rioux@hotmail.com>
2017-11-28 21:37:29 -0500

    Update areFollowingPatterns.py

--- areFollowingPatterns.py
+++ areFollowingPatterns.py
@@ -1,13 +1,13 @@
-Given an array strings, determine whether it follows the sequence given in the patterns array. 
-In other words, there should be no i and j for which strings[i] = strings[j] and patterns[i] ≠ patterns[j] 
-or for which strings[i] ≠ strings[j] and patterns[i] = patterns[j].
+# Given an array strings, determine whether it follows the sequence given in the patterns array. 
+# In other words, there should be no i and j for which strings[i] = strings[j] and patterns[i] ≠ patterns[j] 
+# or for which strings[i] ≠ strings[j] and patterns[i] = patterns[j].
 
-Example:
+# Example:
 
-For strings = ["cat", "dog", "dog"] and patterns = ["a", "b", "b"], the output should be
-areFollowingPatterns(strings, patterns) = true;
-For strings = ["cat", "dog", "doggy"] and patterns = ["a", "b", "b"], the output should be
-areFollowingPatterns(strings, patterns) = false.
+# For strings = ["cat", "dog", "dog"] and patterns = ["a", "b", "b"], the output should be
+# areFollowingPatterns(strings, patterns) = true;
+# For strings = ["cat", "dog", "doggy"] and patterns = ["a", "b", "b"], the output should be
+# areFollowingPatterns(strings, patterns) = false.
 
 
 
